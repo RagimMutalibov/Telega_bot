c60ba55892484a84c8f5c31dae6b3b7f5bf20194
RagimMutalibov <66872554+RagimMutalibov@users.noreply.github.com>
2024-11-01 23:23:59 +0300

    1_

---
+++ 1_3.py
@@ -0,0 +1,15 @@
+tasks = {}
+
+date1 = input("Введите дату: ")
+task1 = input("Введите задачу: ")
+tasks[date1] = task1
+
+date2 = input("Введите дату: ")
+task2 = input("Введите задачу: ")
+tasks[date2] = task2
+
+date3 = input("Введите дату: ")
+task3 = input("Введите задачу: ")
+tasks[date3] = task3
+
+# print(tasks)   
